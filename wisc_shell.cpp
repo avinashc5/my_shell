2658238b4316b3e2632cc6d97cc2416fa8f2d68d
avinashc5 <avinashchaudhari5605@gmail.com>
2024-12-13 21:05:05 +0530

    wisc_shell.cpp works fine. accepts multiple commands too

--- wisc_shell.cpp
+++ wisc_shell.cpp
@@ -13,36 +13,47 @@ int main(){
 		getline(std::cin, line);
 		char* line_c = new char[line.size() + 1];
 		strcpy(line_c, line.c_str());
-		char* pch = strtok(line_c, " ");
-		std::vector<char*> command;
-		while (pch != NULL){
-			command.push_back(pch);
-			if (pch[0] == 'q' && pch[1] == 'u' && pch[2] == 'i' && pch[3] == 't'){
-				RUN = false;
-			}
-			pch = strtok(NULL, " ");
-		}
-		command.push_back(NULL);
-		char* a_command[command.size()];
-		for (int i = 0; i < command.size(); i++){
-			a_command[i] = command[i];
-		}
-		if (!RUN) break;
-		pid_t c_pid = fork();
-		int status;
-		if (c_pid == -1){
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (c_pid > 0){ // Parent process
-			waitpid(c_pid, &status, 0);
-		}
-		else { //Child Process
-			int status_code = execvp(a_command[0], a_command);
 
-			if (status_code == -1){
-				perror("Error");
+		char* pch = strtok(line_c, ";");
+		std::vector<char*> commands;
+		while (pch != NULL){
+			commands.push_back(pch);
+			pch = strtok(NULL, ";");
+		}
+
+		std::vector<std::vector<char*>> command_args(commands.size(), std::vector<char*>());
+		for (int i = 0; i < commands.size(); i++){
+			pch = strtok(commands[i], " ");
+			while (pch != NULL){
+				command_args[i].push_back(pch);
+				if (pch[0] == 'q' && pch[1] == 'u' && pch[2] == 'i' && pch[3] == 't'){
+					RUN = false;
+				}
+				pch = strtok(NULL, " ");
+			}
+			command_args[i].push_back(NULL);
+		}
+
+		if (!RUN) break;
+
+		for (int i = 0; i < command_args.size(); i++){
+			pid_t c_pid = fork();
+			if (c_pid == -1){
+				perror("fork");
 				exit(EXIT_FAILURE);
+			}
+			else if (c_pid > 0){ // Parent process
+				int status;
+				waitpid(c_pid, &status, 0);
+			}
+			else if (c_pid == 0){ // Child process
+				int status_code = execvp(command_args[i][0], command_args[i].data());
+				if (status_code == -1){
+					perror("Error");
+					exit(EXIT_FAILURE);
+				}
+
+				exit(EXIT_SUCCESS);
 			}
 		}
 	}
@@ -56,4 +67,5 @@ mkdir
 rm
 rmdir
 quit
+echo
 */
